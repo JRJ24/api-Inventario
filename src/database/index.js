8141015a797d3ca9b00c10b6527442761f76e6a5
Jonaifry <rjstudios24012006@gmail.com>
2025-09-04 01:28:40 -0400

    DatabaseMongoDB

--- src/database/index.js
+++ src/database/index.js
@@ -1,4 +1,4 @@
-const { MongoClient } = require("mongodb");
+const mongoose = require("mongoose");
 const debug = require("debug")("app:database");
 
 const {config} = require("../config/index");
@@ -8,19 +8,12 @@ var connection = null;
 module.exports.Database = (collection) => 
     new Promise( async(res, rej) => {
     try{
-        if(!connection){
-            const client = new MongoClient(config.dbUri, {
-                tls: true,
-                tlsAllowInvalidCertificates: false,
-                tlsAllowInvalidHostnames: false,
-                serverSelectionTimeoutMS: 5000,
-                connectTimeoutMS: 10000,
-            });
-            connection = await client.connect();
+        mongoose.connect(process.env.DB_URI);
+        const db = mongoose.connection;
+        db.on('error', console.error.bind(console, 'connection error:'));
+        db.once('open', () => {
             debug("Database connected");
-        }
-        debug("Database already connected");
-        const db = connection.db(config.mongoDbName);
+        });
         res(db.collection(collection));
 
     }catch(error){
