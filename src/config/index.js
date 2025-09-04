7d5cd14fb0c0e3049909b97fb5d7a705c3c4dc74
Jonaifry <rjstudios24012006@gmail.com>
2025-09-04 01:35:32 -0400

    Fix mongoose database connection

--- src/config/index.js
+++ src/config/index.js
@@ -2,6 +2,5 @@ require("dotenv").config();
 
 module.exports.config = {
     port: process.env.PORT,
-    dbUri: process.env.DB_URI,
-    mongoDbName: process.env.MONGO_DB_NAME
+    dbUri: process.env.DB_URI
 }
--- src/database/index.js
+++ src/database/index.js
@@ -8,12 +8,11 @@ var connection = null;
 module.exports.Database = (collection) => 
     new Promise( async(res, rej) => {
     try{
-        mongoose.connect(process.env.DB_URI);
-        const db = mongoose.connection;
-        db.on('error', console.error.bind(console, 'connection error:'));
-        db.once('open', () => {
+        if(!connection){
+            connection = await mongoose.connect(config.dbUri);
             debug("Database connected");
-        });
+        }
+        const db = mongoose.connection.db;
         res(db.collection(collection));
 
     }catch(error){
